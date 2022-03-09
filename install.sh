94feb54ef35da3178a57ccf7dcde549859d88649
Dzk <91489656+Dzancok@users.noreply.github.com>
2022-03-09 16:26:03 +0700

    Delete install.sh

--- install.sh
+++
@@ -1,8 +0,0 @@
-apt update && pkg upgrade
-apt install git
-apt install nodejs
-apt install ffmpeg
-apt install imagemagick
-apt install yarn
-git clone https://github.com/Dzancok/DzkBOT-Md
-cd DzkBOT-Md
